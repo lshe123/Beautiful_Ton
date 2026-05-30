46ba152b0c5ef173445c29ac1b192c8f4ef9b99b
lshe123 <lsh96619@gmail.com>
2026-05-30 16:20:34 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-Enterimport { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
+  },
+  build: {
+    chunkSizeWarningLimit: 2000,
+    // هذا السطر يخبر Vite و Vercel أن ملف index.html موجود في الجذر مباشرة
+    rollupOptions: {
+      input: './index.html'
+    }
   }
 });
